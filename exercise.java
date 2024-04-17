72953b05d6cf7da145f8f5169e19a7ad3e7775c3
AiShangYuWen <146498837+Zxlove720@users.noreply.github.com>
2024-04-17 22:30:23 +0800

    Add files via upload
    2024.4.17.java.exercise

---
+++ exercise.java
@@ -0,0 +1,14 @@
+package demo1;
+import java.util.Scanner;
+public class exercise {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("hello world");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b);
+    }
+
+}
